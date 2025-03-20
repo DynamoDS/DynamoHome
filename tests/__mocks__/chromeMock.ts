e1ffcf45cfc249a9af8f5476a01ab7d6bfc21c46
Deyan Nenov <dnenov@archilizer.com>
2025-03-20 18:42:28 +0000

    version bump
    - bumped to 1.0.23
    - fixed failing tests

--- tests/__mocks__/chromeMock.ts
+++ tests/__mocks__/chromeMock.ts
@@ -2,7 +2,17 @@ export const chromeMock = {
   webview: {
     hostObjects: {
       scriptObject: {
+        SaveSettings: jest.fn(),
         ApplicationLoaded: jest.fn(),
+        OpenFile: jest.fn(),
+        StartGuidedTour: jest.fn(),
+        OpenWorkspace: jest.fn(),
+        ShowTempalte: jest.fn(),
+        ShowBackupFilesInFolder: jest.fn(),
+        NewWorkspace: jest.fn(),
+        NewCustomNodeWorkspace: jest.fn(),
+        ShowSampleFilesInFolder: jest.fn(),
+        ShowSampleDatasetsInFolder: jest.fn(),
       },
     },
   },
